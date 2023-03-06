7827803a154a894b0802e20c6bc5239769ad58a6
gabriel Morais <gabrielmmorais.juliao@gmail.com>
2023-03-06 16:42:03 -0300

    commit vercel.json

--- api/index.php
+++ api/index.php
@@ -2,8 +2,8 @@
 
 session_start();
 require('vendor/autoload.php');
-define('INCLUDE_PATH_STATIC','https://redesocialcosplay.vercel.app/api/RedeSocialCosplay/Views/pages/');
-define('INCLUDE_PATH','https://redesocialcosplay.vercel.app/api/');
+define('INCLUDE_PATH_STATIC','https://redesocialcosplay.000webhostapp.com/RedeSocialCosplay/Views/pages/');
+define('INCLUDE_PATH','https://redesocialcosplay.000webhostapp.com/');
 $app = new RedeSocialCosplay\Aplicacao();
 $app->run();
 
