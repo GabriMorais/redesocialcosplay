9e5dc06fbe64c2bc4eaddd8021c82e6c686714e3
gabriel Morais <gabrielmmorais.juliao@gmail.com>
2023-03-06 14:19:20 -0300

    commit alteração do arquivo index.php

--- api/index.php
+++ api/index.php
@@ -2,8 +2,8 @@
 
 session_start();
 require('vendor/autoload.php');
-define('INCLUDE_PATH_STATIC','http://localhost/cosplay/api/RedeSocialCosplay/Views/pages/');
-define('INCLUDE_PATH','http://localhost/cosplay/api/');
+define('INCLUDE_PATH_STATIC','https://redesocialcosplay.vercel.app/api/RedeSocialCosplay/Views/pages/');
+define('INCLUDE_PATH','https://redesocialcosplay.vercel.app/api/');
 $app = new RedeSocialCosplay\Aplicacao();
 $app->run();
 
